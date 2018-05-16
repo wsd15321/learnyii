c249fbd676739cc51bc836c9a8558ccd97d63d4f
ihnnfnnbsa <3444157027@qq.com>
2018-05-16 18:24:07 +0800

    update

---
+++ vendor/yiisoft/yii2/base/Appllication.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sc
+ * Date: 2018/5/16/016
+ * Time: 15:42
+ */
+
+namespace yii\base;
+
+
+abstract class Appllication {
+    
+}
---
+++ vendor/yiisoft/yii2/base/Module.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sc
+ * Date: 2018/5/16/016
+ * Time: 15:59
+ */
+
+namespace yii\base;
+
+use yii\di\ServiceLocator;
+use Yii;
+
+class Module extends ServiceLocator
+{
+
+    public $params = [];
+
+    public $id;
+
+    public $model;
+    
+
+
+
+
+}
