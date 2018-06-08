b72544f6f321fead2511098dffe9fc2d7049ba4c
ihnnfnnbsa <3444157027@qq.com>
2018-06-08 12:52:07 +0800

    test

---
+++ module/models/AgetA.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sc
+ * Date: 2018/6/8/008
+ * Time: 12:49
+ */
+
+namespace module\models;
+
+
+use yii\base\Behavior;
+
+class AgetA extends Behavior
+{
+
+    public function getAbc()
+    {
+        return 'get AgetA()';
+    }
+
+}
--- module/models/TestBehavior.php
+++ module/models/TestBehavior.php
@@ -24,7 +24,7 @@ class TestBehavior extends Component
                 'owner' => $this
             ],
             'ok' => [
-
+                'class' => 'module\models\AgetA'
             ],
         ];
     }
